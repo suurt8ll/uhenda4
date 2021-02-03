2e3f08f185e3a0c521b876b23e688e106cfa72ae
Meleon420 <46417608+Meleon420@users.noreply.github.com>
2021-02-03 22:38:08 +0200

    ShapeDrawer lib, Gradle update

--- core/src/com/connectfour/screens/gameScreens/ConnectFourScreen.java
+++ core/src/com/connectfour/screens/gameScreens/ConnectFourScreen.java
@@ -1,26 +1,42 @@
 package com.connectfour.screens.gameScreens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.connectfour.Games;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class ConnectFourScreen implements Screen {
-    private Games game;
+
+    private final Games game;
     private Stage stage;
+    private ShapeDrawer shapeDrawer;
 
     public ConnectFourScreen(final Games game){
         this.game = game;
-        stage = new Stage(game.viewport,game.batch);
     }
 
     @Override
     public void show() {
-
+        this.stage = new Stage(game.viewport, game.batch);
+        System.out.println("negger");
+        Pixmap whitesq = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        whitesq.setColor(1,1,1,1);
+        whitesq.fillRectangle(0,0,1,1);
+        this.shapeDrawer = new ShapeDrawer(game.batch, new TextureRegion(new Texture(whitesq)));
     }
 
     @Override
     public void render(float delta) {
-
+        this.stage.act();
+        this.stage.draw();
+        game.batch.begin();
+        shapeDrawer.line(0,0, 100, 100, Color.RED, 2);
+        shapeDrawer.filledEllipse(100, 100, 200, 200, 0, Color.RED, Color.BLACK);
+        game.batch.end();
     }
 
     @Override
@@ -45,6 +61,6 @@ public class ConnectFourScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
