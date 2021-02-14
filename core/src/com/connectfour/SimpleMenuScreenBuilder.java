3b182eae472b09201f71c9c998421daee39ac8bd
Meleon420 <46417608+Meleon420@users.noreply.github.com>
2021-02-14 11:20:46 +0200

    screenbuilder frameworki kallal nokitsemine
    Jätan frameworki tegemise pooleli, sest see oleks liiga ajakulukas. Nüüd hakkan multiplayerit looma.

---
+++ core/src/com/connectfour/SimpleMenuScreenBuilder.java
@@ -0,0 +1,115 @@
+package com.connectfour;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+
+public class SimpleMenuScreenBuilder {
+
+    private final Games game;
+    private float menuWidth;
+    private float menuHeight;
+    private int rows = 0;
+
+    private final ArrayList<Float> rowLayout;
+    //TODO Parem alternatiiv HashMapidele oleks rohkem memory efficient.
+    private final HashMap<Integer, ArrayList<Float>> columnLayout;
+    private final HashMap<Integer, ArrayList<Actor>> actorsMatrix;
+
+    private Stage stage;
+
+    public SimpleMenuScreenBuilder(Games game) {
+        this.game = game;
+        rowLayout = new ArrayList<>();
+        columnLayout = new HashMap<>();
+        actorsMatrix = new HashMap<>();
+    }
+
+    public void newEmptyColumn(int rowNumber, float widthPercentage) {
+        newColumn(rowNumber, widthPercentage, null, null, false);
+    }
+
+    /**Kogu ekraani kohandab ennast selle veeru järgi.*/
+    public void newAncorColumn(int rowNumber, float widthPercentage, Actor actor) {
+        newColumn(rowNumber, widthPercentage, actor, null, true);
+    }
+
+    public void newColumn(int rowNumber, float widthPercentage, Actor actor, Alignment verticalAligment) {
+        newColumn(rowNumber, widthPercentage, actor, verticalAligment, false);
+    }
+
+    private void newColumn(int rowNumber, float widthPercentage, Actor actor, Alignment verticalAlignment, boolean ancor) {
+        if (ancor) {
+            menuWidth = actor.getWidth() / widthPercentage;
+            menuHeight = actor.getHeight() / rowLayout.get(rowNumber);
+        }
+        columnLayout.get(rowNumber - 1).add(widthPercentage);
+        actorsMatrix.get(rowNumber - 1).add(actor);
+    }
+
+    public void newRow(float heightPercentage) {
+        rowLayout.add(heightPercentage);
+        columnLayout.put(rows + 1, new ArrayList<>());
+        actorsMatrix.put(rows + 1, new ArrayList<>());
+        rows += 1;
+    }
+
+    public void finish() {
+        //Kontrollib, kas kõigi ridade ja veergude summa on 100%.
+        float sumY = 0;
+        for (int reaNumber = 0; reaNumber < rowLayout.size(); reaNumber++) {
+            sumY += rowLayout.get(reaNumber);
+            float sumX = 0;
+            ArrayList<Float> row = columnLayout.get(reaNumber);
+            for (Float percentage : row) {
+                sumX += percentage;
+            }
+            //sumX < 100% --> lisa lõpu täiteks uus tühi veerg
+            //sumX > 100% --> kärbi veerge
+            if (sumX < 1) {
+                row.add(1 - sumX);
+                actorsMatrix.get(reaNumber).add(null);
+            } else if (sumX > 1) {
+                ListIterator<Float> floatIterator = row.listIterator(row.size());
+                int j = row.size() - 1;
+                while (floatIterator.hasPrevious()) {
+                    Float veerg = floatIterator.previous();
+                    if (sumX - veerg > 1) {
+                        row.remove(j);
+                        actorsMatrix.get(reaNumber).remove(j);
+                        sumX -= veerg;
+                    } else if (sumX - veerg < 1) {
+                        row.set(j, 1 - (sumX - veerg));
+                        break;
+                    }
+                    j-=1;
+                }
+            }
+            //TODO Ridade automaatne kärpimine.
+            reaNumber += 1;
+        }
+        //Tühja Stage loomine.
+        Camera cam = new OrthographicCamera();
+        cam.position.set(menuWidth / 2, menuHeight / 2, 0);
+        Viewport viewport = new ExtendViewport(menuWidth, menuHeight, cam);
+        game.batch.setProjectionMatrix(stage.getViewport().getCamera().combined);
+        stage = new Stage(viewport, game.batch);
+
+        //TODO Actorite lisamine ja scalemine Stagesse.
+
+    }
+
+    public enum Alignment {
+        RIGHT,
+        LEFT,
+        CENTERED,
+    }
+
+}
